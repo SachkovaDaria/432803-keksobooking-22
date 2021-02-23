94e632a67a2977ea826efe528587484e0d00fd33
BankinaDasha <bankinadasha@gmail.com>
2021-02-23 22:07:10 +0300

    Помощь друга1

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 /* global L:readonly */
 import {createAds} from './utils.js';
-import {createCardElements, mapElement} from './card.js';
+import {createCardElements, createCardElement, mapElement} from './card.js';
 import {map, mapTileElement, markerIcon, mapMarkerMainElelment} from './map.js';
 
 const ads = createAds();
 const cardElements = createCardElements(ads);
 
 mapTileElement.addTo(map);
-
 mapMarkerMainElelment.addTo(map);
+
 
 ads.forEach((ad) => {
   const lat = ad.location.x;
@@ -20,5 +20,9 @@ ads.forEach((ad) => {
     icon: markerIcon,
   },
   );
-  marker.addTo(map);
+  marker
+    .addTo(map)
+    .bindPopup(
+      createCardElement(ad),
+    );
 });
