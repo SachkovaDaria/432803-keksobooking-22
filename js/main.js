ec42cbe1d3d6a54e3a2c885f87028dfcccfdc583
BankinaDasha <bankinadasha@gmail.com>
2021-02-26 21:04:28 +0300

    Помощь друга

--- js/main.js
+++ js/main.js
@@ -1,28 +1,10 @@
-/* global L:readonly */
 import {createAds} from './utils.js';
-import {createCardElement} from './card.js';
-import {map, mapTileElement, markerIcon, mapMarkerMainElelment} from './map.js';
+// import {createCardElement} from './card.js';
+import {getInactiveMap, initMap, addMarkersToMap} from './map.js';
 
 const ads = createAds();
 // const cardElements = createCardElements(ads);
 
-mapTileElement.addTo(map);
-mapMarkerMainElelment.addTo(map);
-
-
-ads.forEach((ad) => {
-  const lat = ad.location.x;
-  const lng = ad.location.y;
-  const marker = L.marker({
-    lat,
-    lng,
-  }, {
-    icon: markerIcon,
-  },
-  );
-  marker
-    .addTo(map)
-    .bindPopup(
-      createCardElement(ad),
-    );
-});
+getInactiveMap();
+initMap();
+addMarkersToMap(ads);
--- js/map.js
+++ js/map.js
@@ -1,77 +1,107 @@
 /* global L:readonly */
+import {createCardElement} from './card.js';
 
-// неактивное состояние карты
 const formElement = document.querySelector('.ad-form');
 const fieldsetElements = formElement.querySelectorAll('fieldset');
-formElement.classList.add('ad-form--disabled');
-fieldsetElements.forEach((fieldset)=> {
-  fieldset.setAttribute('disabled', 'disabled');
-});
 const filterElement = document.querySelector('.map__filters');
 const filterElements = filterElement.querySelectorAll('.map__filter');
-filterElement.classList.add('map__filters--disabled');
-filterElements.forEach((filter)=> {
-  filter.setAttribute('disabled', 'disabled');
-});
 
-//слушатель события load «инициализация карты»
+// неактивное состояние карты
+const getInactiveMap = () => {
+  formElement.classList.add('ad-form--disabled');
+  fieldsetElements.forEach((fieldset)=> {
+    fieldset.disabled = true;
+  });
+  filterElement.classList.add('map__filters--disabled');
+  filterElements.forEach((filter)=> {
+    filter.disabled = true;
+  });
+}
+//активное состояние карты
 const map = L.map('map-canvas')
-  .on('load', () => {
+const initMap = () => {
+  map.addEventListener('load', () => {
     formElement.classList.remove('ad-form--disabled');
     fieldsetElements.forEach((fieldset)=> {
-      fieldset.removeAttribute('disabled', 'disabled');
+      fieldset.disabled = false;
     });
     filterElement.classList.remove('map__filters--disabled');
     filterElements.forEach((filter)=> {
-      filter.removeAttribute('disabled', 'disabled');
+      filter.disabled = false;
     });
   })
-  .setView({
-    lat: 35.6894,
-    lng: 139.692,
-  }, 10);
+    .setView({
+      lat: 35.6894,
+      lng: 139.692,
+    }, 10);
 
-const mapTileElement = L.tileLayer(
-  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-  {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  },
-);
+  const mapTile = L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    },
+  );
 
+  mapTile.addTo(map);
 
-//добавляет main метку
-const mainMarkerIcon = L.icon({
-  iconUrl: '../img/main-pin.svg',
-  iconSize: [52, 52],
-  iconAnchor: [26, 52],
-});
+  //добавляет main метку
+  const mainMarkerIcon = L.icon({
+    iconUrl: '../img/main-pin.svg',
+    iconSize: [52, 52],
+    iconAnchor: [26, 52],
+  });
 
-const mapMarkerMainElelment = L.marker(
-  {
-    lat: 35.6894,
-    lng: 139.692,
-  },
-  {
-    draggable: true,
-    icon: mainMarkerIcon,
-  },
-);
+  const mapMarkerMain = L.marker(
+    {
+      lat: 35.6894,
+      lng: 139.692,
+    },
+    {
+      draggable: true,
+      icon: mainMarkerIcon,
+    },
+  );
 
-const address = document.querySelector('#address');
-address.setAttribute('readonly', 'readonly');
-address.value = `${mapMarkerMainElelment.getLatLng().lat.toFixed(5)}, ${mapMarkerMainElelment.getLatLng().lng.toFixed(5)}`;
-mapMarkerMainElelment.on('moveend', (evt) => {
-  address.value = `${evt.target.getLatLng().lat.toFixed(5)}, ${evt.target.getLatLng().lng.toFixed(5)}`;
-});
+  mapMarkerMain.addTo(map);
 
-
-//добавляет обычную метку
+  const address = document.querySelector('#address');
+  address.readOnly = true;
+  const setAdresOnMap = (addressX,addressY) => {
+    address.value =`${addressX}, ${addressY}`;
+  }
+  const x = mapMarkerMain.getLatLng().lat.toFixed(5);
+  const y = mapMarkerMain.getLatLng().lng.toFixed(5);
+  setAdresOnMap(x,y);
+  mapMarkerMain.addEventListener('moveend', (evt) => {
+    const addressX = evt.target.getLatLng().lat.toFixed(5);
+    const addressY = evt.target.getLatLng().lng.toFixed(5);
+    setAdresOnMap (addressX,addressY)
+  });
+}
+//обычная метка
 const markerIcon = L.icon({
   iconUrl: '../img/pin.svg',
   iconSize: [40, 40],
   iconAnchor: [20, 40],
 });
+//добавляет обычные метки на карту
+const addMarkersToMap = (ads) => {
+  ads.forEach((ad) => {
+    const lat = ad.location.x;
+    const lng = ad.location.y;
+    const marker = L.marker({
+      lat,
+      lng,
+    }, {
+      icon: markerIcon,
+    },
+    );
+    marker
+      .addTo(map)
+      .bindPopup(
+        createCardElement(ad),
+      );
+  });
+}
 
-
-
-export {map , mapTileElement, markerIcon, mapMarkerMainElelment};
+export {getInactiveMap, initMap, addMarkersToMap};
